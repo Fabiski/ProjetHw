561c62e10aca3701e6e05756ef3b837ffa5af435
Fabiski <fabrice.renault@edu.devinci.fr>
2024-10-24 16:28:48 +0200

    ChoixPin allumé

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 #![no_main]
 
 use cortex_m_rt::entry;
+use core::panic::PanicInfo;
 
 
 const PORTB: *mut u8 = 0x25 as *mut u8; // PORTB address
@@ -10,17 +11,18 @@ const PORTD: *mut u8 = 0x2B as *mut u8; // PORTD address
 const DDRD: *mut u8 = 0x2A as *mut u8;  // DDRD address
 const PINB: *mut u8 = 0x23 as *mut u8;  //PINB address
 const PIND: *mut u8 = 0x29 as *mut u8;  //PIND address
-const SOURCE: *mut u8 = 0b00000001 as *mut u8;  // SOURCE (chosen port, will change in the switch) 
-const CHOICED: *mut u8;
-const CHOICEP: *mut u8;
-const CHOICEPIN: *mut u8;
+static mut SOURCE: u8 = 0b00000001;
+static mut CHOICED: *mut u8 = 0x2A as *mut u8;
+static mut CHOICEP: *mut u8 = 0x2B as *mut u8;
+static mut CHOICEPIN: *mut u8 = 0x29 as *mut u8;
 
 //https://tenor.com/view/rust-femboy-rust-femboy-programming-rust-programming-gif-27321790
 
 #[entry]
 fn main() -> ! {
 
-    let num=3;
+    let num=2;
+    unsafe{
     match num{
      0=>{SOURCE = 0b00000001;CHOICED = DDRD; CHOICEP = PORTD; CHOICEPIN = PIND;},   
      1=>{SOURCE = 0b00000010;CHOICED = DDRD; CHOICEP = PORTD; CHOICEPIN = PIND;},
@@ -36,34 +38,20 @@ fn main() -> ! {
      11=>{SOURCE = 0b00001000;CHOICED = DDRB; CHOICEP = PORTB; CHOICEPIN = PINB;},
      12=>{SOURCE = 0b00010000;CHOICED = DDRB; CHOICEP = PORTB; CHOICEPIN = PINB;},
      13=>{SOURCE = 0b00100000;CHOICED = DDRB; CHOICEP = PORTB; CHOICEPIN = PINB;},
-     _=>println!("Raté"),
+     _=>panic!("Raté"),
+    }
     }
     loop {
         unsafe {
-        // Infinite loop
-            loop {
-            // Turn on the LED (set bit 5 in PORTB)
-                core::ptr::write_volatile(CHOICED,SOURCE);
-            /*loop {
-                for _ in 0..1_000_000 
-                {
-                    core::ptr::write_volatile(PORTB, 0b00100000);
-                }
-                for _ in 0..100_000
-                {
-                    core::ptr::write_volatile(PORTB, 0b00000000);
-                }
-            }*/
-                let pinb = core::ptr::read_volatile(PINB);
-                let port13_high = pinb & 0b00100000; // Vérifier si PB5 (bit 5) est à 1
-
-                if port13_high != 0 {
-                    // Si port 13 est HIGH, mettre port 12 (PB4) à HIGH
-                    core::ptr::write_volatile(CHOICEP, SOURCE); // PB4 = 1
-                } else {
-                    // Sinon, mettre port 12 (PB4) à LOW
-                    core::ptr::write_volatile(CHOICEP, 0b00000000); // PB4 = 0
-                }
+            core::ptr::write_volatile(CHOICED,SOURCE);
+            //core::ptr::write_volatile(CHOICEP, SOURCE);
+            for _ in 0..1_000_000 
+            {
+                core::ptr::write_volatile(CHOICEP, SOURCE);
+            }
+            for _ in 0..1_000_000
+            {
+                core::ptr::write_volatile(CHOICEP, 0b00000000);
             }
         }
     }
